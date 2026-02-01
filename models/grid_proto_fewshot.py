99ac8b4ca31573584ddf1605f3f54e97e077b462
Bao Nguyen <nguyenngocbaocmt@gmail.com>
2026-02-02 01:31:04 +0800

    fix misalign bug

--- models/grid_proto_fewshot.py
+++ models/grid_proto_fewshot.py
@@ -145,13 +145,13 @@ class FewShotSeg(nn.Module):
         fore_mask_resized = fore_mask_resized.view(Wa, Sh, Raters, *fts_size).squeeze(3)  # Wa x Sh x R x H' x W'
         back_mask_resized = back_mask_resized.view(Wa, Sh, Raters, *fts_size).squeeze(3)  # Wa x Sh x R x H' x W'
 
-        # consensus mask from resized fore masks for seed init
-        fore_mask_consensus = (fore_mask_resized.mean(dim=2) > 0.5).float()  # Wa x Sh x H' x W'
-        s_y = fore_mask_consensus[0, 0]
+        # consensus mask from fore masks for seed init
+        fore_mask_consensus = (fore_mask_stacked.mean(dim=2) > 0.5).float()  # Wa x Sh x H x W
+        s_y = fore_mask_consensus[0][0] # H x W
 
         init_seed_list = []
-        mask = (s_y == 1).float()  # H' x W'
-
+        mask = (s_y == 1).float()  # H x W
+        breakpoint()
         init_seed = place_seed_points_d(mask, down_stride=8, max_num_sp=5, avg_sp_area=100)
         init_seed_list.append(init_seed.unsqueeze(0))
         s_init_seed = torch.cat(init_seed_list).cuda()
